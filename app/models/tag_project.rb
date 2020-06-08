0915c110fe4a3f1daf5c16cd64b39e9fc8b7d7d2
Jonathan Fischer <skelothan@gmail.com>
2020-06-07 23:59:21 -0400

    Wrote tag_project model

--- app/models/tag_project.rb
+++ app/models/tag_project.rb
@@ -1,4 +1,7 @@
 class TagProject < ApplicationRecord
   belongs_to :project
   belongs_to :tag
+
+  # Validations
+  validates_uniqueness_of :project_id, scope: :tag_id, message: "This project already has that tag."
 end
