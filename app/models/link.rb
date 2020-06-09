ec963f528e506866c26329843c4d47d501871761
Jonathan Fischer <skelothan@gmail.com>
2020-06-08 20:35:10 -0400

    Wrote link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,20 @@
 class Link < ApplicationRecord
-  belongs_to :project
+    belongs_to :project
+
+    # Validations
+    validates_uniqueness_of :url, scope: :project_id, message: "This project already has that link."
+
+    # Scopes
+    scope :for_project,     lambda(p) {where('project_id = ?', p.id)}
+
+    # Methods
+    def activate()
+        self.active = true
+        self.save!
+    end
+
+    def deactivate()
+        self.active = false
+        self.save!
+    end
 end
